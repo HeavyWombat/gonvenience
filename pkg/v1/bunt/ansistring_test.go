7507c2e3241977200837a7c5e274d3992a423ce0
Matthias Diester <mddiester@gmail.com>
2018-11-18 20:45:38 +0100

    Add Sprintf tests
    Add Sprintf based tests to the new ANSI string test cases.

--- pkg/v1/bunt/ansistring_test.go
+++ pkg/v1/bunt/ansistring_test.go
@@ -35,66 +35,77 @@ var _ = Describe("ANSI string tests", func() {
 		TrueColorSetting = ON
 	})
 
-	It("should preserve already existing styles in an input string", func() {
-		alreadyStyledText := "Preserve styles like \x1b[1mbold\x1b[0m, \x1b[3mitalic\x1b[0m, \x1b[1;38;2;255;128;16mcolors\x1b[0m, and \x1b[4;38;2;128;128;128mstyled colors\x1b[0m in input strings."
-		Expect(Sprint(alreadyStyledText)).To(BeEquivalentTo(alreadyStyledText))
+	Context("Testing Sprint wrapper function", func() {
+		It("should preserve already existing styles in an input string in Sprint function", func() {
+			alreadyStyledText := "Preserve styles like \x1b[1mbold\x1b[0m, \x1b[3mitalic\x1b[0m, \x1b[1;38;2;255;128;16mcolors\x1b[0m, and \x1b[4;38;2;128;128;128mstyled colors\x1b[0m in input strings."
+			Expect(Sprint(alreadyStyledText)).To(BeEquivalentTo(alreadyStyledText))
+		})
+
+		It("should translate *bold* syntax into styled text in Sprint function", func() {
+			Expect(Sprint(">>> *bold* <<<")).To(BeEquivalentTo(
+				fmt.Sprintf(">>> %s <<<",
+					Style("bold", Bold),
+				)))
+		})
+
+		It("should translate _italic_ syntax into styled text in Sprint function", func() {
+			Expect(Sprint(">>> _italic_ <<<")).To(BeEquivalentTo(
+				fmt.Sprintf(">>> %s <<<",
+					Style("italic", Italic),
+				)))
+		})
+
+		It("should translate ~underline~ syntax into styled text in Sprint function", func() {
+			Expect(Sprint(">>> ~underline~ <<<")).To(BeEquivalentTo(
+				fmt.Sprintf(">>> %s <<<",
+					Style("underline", Underline),
+				)))
+		})
+
+		It("should translate Blue{Blue} color syntax into styled text in Sprint function", func() {
+			Expect(Sprint(">>> Blue{Blue} <<<")).To(BeEquivalentTo(
+				fmt.Sprintf(">>> %s <<<",
+					Colorize("Blue", Blue),
+				)))
+		})
+
+		It("should translate multiple occurrences of *bold* into styled text in Sprint function", func() {
+			Expect(Sprint("Text *with* multiple *bold* elements.")).To(BeEquivalentTo(
+				fmt.Sprintf("Text %s multiple %s elements.",
+					Style("with", Bold),
+					Style("bold", Bold),
+				)))
+		})
+
+		It("should translate *bold* and _italic_ syntax into styled text in Sprint function", func() {
+			Expect(Sprint("Use *bold* and _italic_ in texts.")).To(BeEquivalentTo(
+				fmt.Sprintf("Use %s and %s in texts.",
+					Style("bold", Bold),
+					Style("italic", Italic),
+				)))
+		})
+
+		It("should translate nested text marker in color marker into the correct output in Sprint function", func() {
+			Expect(Sprint(" --- >>>CornflowerBlue{~http://github.com~}<<< ---")).To(BeEquivalentTo(
+				fmt.Sprintf(" --- >>>\x1b[38;2;100;149;237m\x1b[4m%s\x1b[0m\x1b[0m<<< ---",
+					"http://github.com",
+				)))
+		})
+
+		It("should translate nested color marker in text marker into the correct output in Sprint function", func() {
+			Expect(Sprint(" --- >>>~CornflowerBlue{http://github.com}~<<< ---")).To(BeEquivalentTo(
+				fmt.Sprintf(" --- >>>\x1b[4m\x1b[38;2;100;149;237m%s\x1b[0m\x1b[0m<<< ---",
+					"http://github.com",
+				)))
+		})
 	})
 
-	It("should translate *bold* syntax into styled text", func() {
-		Expect(Sprint(">>> *bold* <<<")).To(BeEquivalentTo(
-			fmt.Sprintf(">>> %s <<<",
-				Style("bold", Bold),
-			)))
-	})
-
-	It("should translate _italic_ syntax into styled text", func() {
-		Expect(Sprint(">>> _italic_ <<<")).To(BeEquivalentTo(
-			fmt.Sprintf(">>> %s <<<",
-				Style("italic", Italic),
-			)))
-	})
-
-	It("should translate ~underline~ syntax into styled text", func() {
-		Expect(Sprint(">>> ~underline~ <<<")).To(BeEquivalentTo(
-			fmt.Sprintf(">>> %s <<<",
-				Style("underline", Underline),
-			)))
-	})
-
-	It("should translate Blue{Blue} color syntax into styled text", func() {
-		Expect(Sprint(">>> Blue{Blue} <<<")).To(BeEquivalentTo(
-			fmt.Sprintf(">>> %s <<<",
-				Colorize("Blue", Blue),
-			)))
-	})
-
-	It("should translate multiple occurrences of *bold* into styled text", func() {
-		Expect(Sprint("Text *with* multiple *bold* elements.")).To(BeEquivalentTo(
-			fmt.Sprintf("Text %s multiple %s elements.",
-				Style("with", Bold),
-				Style("bold", Bold),
-			)))
-	})
-
-	It("should translate *bold* and _italic_ syntax into styled text", func() {
-		Expect(Sprint("Use *bold* and _italic_ in texts.")).To(BeEquivalentTo(
-			fmt.Sprintf("Use %s and %s in texts.",
-				Style("bold", Bold),
-				Style("italic", Italic),
-			)))
-	})
-
-	It("should translate nested text marker in color marker into the correct output", func() {
-		Expect(Sprint(" --- >>>CornflowerBlue{~http://github.com~}<<< ---")).To(BeEquivalentTo(
-			fmt.Sprintf(" --- >>>\x1b[38;2;100;149;237m\x1b[4m%s\x1b[0m\x1b[0m<<< ---",
-				"http://github.com",
-			)))
-	})
-
-	It("should translate nested color marker in text marker into the correct output", func() {
-		Expect(Sprint(" --- >>>~CornflowerBlue{http://github.com}~<<< ---")).To(BeEquivalentTo(
-			fmt.Sprintf(" --- >>>\x1b[4m\x1b[38;2;100;149;237m%s\x1b[0m\x1b[0m<<< ---",
-				"http://github.com",
-			)))
+	Context("Testing Sprintf wrapper function", func() {
+		It("should translate *bold* syntax into styled text in Sprintf function", func() {
+			Expect(Sprintf(">>> *%s* <<<", "bold")).To(BeEquivalentTo(
+				fmt.Sprintf(">>> %s <<<",
+					Style("bold", Bold),
+				)))
+		})
 	})
 })
