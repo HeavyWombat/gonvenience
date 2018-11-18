25de2e728b7a62796e3219c12565b7ffad290ae4
Matthias Diester <mddiester@gmail.com>
2018-11-18 22:00:30 +0100

    Move terminal functions from bunt to term
    Move terminal related functions such as `IsTerminal` to `term` package.

--- pkg/v1/term/term.go
+++ pkg/v1/term/term.go
@@ -18,11 +18,16 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+/*
+Package term contains convenience functions for terminal related tasks, for
+example to get the current terminal width or a reasonable fallback.
+*/
 package term
 
 import (
 	"os"
 
+	isatty "github.com/mattn/go-isatty"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
@@ -37,6 +42,18 @@ var FixedTerminalWidth = -1
 
 // FixedTerminalHeight allows a manual fixed override of the terminal height.
 var FixedTerminalHeight = -1
+
+// isDumbTerminal points to true if the current terminal has limited support for
+// escape sequences, false otherwise, or nil if uninitialised.
+var isDumbTerminal *bool
+
+// isTerminal points to true if the current STDOUT stream writes to a terminal
+// (not a redirect), false otherwise, or nil if uninitialised.
+var isTerminal *bool
+
+// isTrueColor points to true if the current terminal reports to support 24-bit
+// colors, false otherwise, or nil if uninitialised.
+var isTrueColor *bool
 
 // GetTerminalWidth return the terminal width (available characters per line)
 func GetTerminalWidth() int {
@@ -79,4 +96,39 @@ func GetTerminalSize() (int, int) {
 	default:
 		return width, height
 	}
+}
+
+// IsDumbTerminal returns whether the current terminal has a limited feature set
+func IsDumbTerminal() bool {
+	if isDumbTerminal == nil {
+		isTermDumbCheck := os.Getenv("TERM") == "dumb"
+		isDumbTerminal = &isTermDumbCheck
+	}
+
+	return *isDumbTerminal
+}
+
+// IsTerminal returns whether this program runs in a terminal and not in a pipe
+func IsTerminal() bool {
+	if isTerminal == nil {
+		isTerminalCheck := isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd())
+		isTerminal = &isTerminalCheck
+	}
+
+	return *isTerminal
+}
+
+// IsTrueColor returns whether the current terminal supports 24 bit colors
+func IsTrueColor() bool {
+	if isTrueColor == nil {
+		var isTrueColorCheck = false
+		switch os.Getenv("COLORTERM") {
+		case "truecolor", "24bit":
+			isTrueColorCheck = true
+		}
+
+		isTrueColor = &isTrueColorCheck
+	}
+
+	return *isTrueColor
 }
